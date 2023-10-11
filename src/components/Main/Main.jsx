02bfb812ed424eaf87f8db70a4e93fffd88e2f44
Pugga <38046181+GoodEgUY@users.noreply.github.com>
2023-10-11 15:35:29 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,33 @@
 import "./main.css";
 
 function Main() {
+  const [leadSource, setLeadSource] = useState("");
+ 
+
+  const botToken = "6469582452:AAFPwZGy8Khu_pS1CuERcCAL99enW2_Cv70";
+  const urlApi = `https://api.telegram.org/bot${botToken}/sendMessage`;
+  const chatId = "-1001922657212";
+  const [success, setSuccess] = useState("");
+
+  const sendAlert = (e, source) => {
+    e.preventDefault();
+    setLeadSource(source)
+    let message = `<b>Новый лид: ${leadSource}</b>\n`;
+  
+    axios
+      .post(urlApi, {
+        chat_id: chatId,
+        parse_mode: "html",
+        text: message,
+      })
+      .then((res) => {
+        setSuccess(true);
+      })
+      .catch((err) => {
+        setSuccess(false);
+      })
+      .finally(() => {});
+  };
   return (
     <>
       <div className="main">
@@ -10,13 +37,13 @@ function Main() {
           <img src="./images/logo.png" alt="" className="ava" />
 
           <div className="dashbord">
-            <a href="https://instagram.com/amy_shop_kharkiv?igshid=MzRlODBiNWFlZA==">
+            <a href="https://instagram.com/amy_shop_kharkiv?igshid=MzRlODBiNWFlZA==" onClick={sendAlert("INSTAGRAM")}>
               <div className="button">
                 <p>INSTAGRAM</p>
                 <img src="./images/inst.png" alt="" className="ico" />
               </div>
             </a>
-            <a href="https://t.me/amy_shop_kh">
+            <a href="https://t.me/amy_shop_kh" onClick={sendAlert("TELEGRAM")}>
               <div className="button">
                 <p>TELEGRAM</p>
 
